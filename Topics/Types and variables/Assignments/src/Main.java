0c9d24255d8667cb65271ed9c2b278720f073e9c
krish082 <kishankumarshaw082@gmail.com>
2022-01-31 22:43:40 +0530

    Initial commit

---
+++ Topics/Types and variables/Assignments/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        final int a = 3;
+        final int b = a + 2;
+        final int c = b - 1;
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
---
+++ Topics/Types and variables/Fill the declaration/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final int variable = 123456;  
+
+        System.out.println(variable);
+    }
+}
